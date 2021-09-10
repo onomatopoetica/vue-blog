eea38072a5d51224ab46a033ff7b6ea120b9d699
Jenn Bates <jenn.bates@nikolamotor.com>
2021-09-10 12:45:57 -0700

    Added prettier + nav bar + mobile styling

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+// Simply settings to convert double quotes to single quotes and semicolons are not automatically inserted
+// the ones that are there will be removed.
+
+module.exports = {
+    singleQuote: true,
+    semi: false
+}
--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,36 @@
 <template>
   <div class="app-wrapper">
     <div class="app">
+      <Navigation />
       <router-view />
     </div>
   </div>
 </template>
 
 <script>
+import Navigation from './components/Navigation'
+
 export default {
-  name: "app",
-  components: {},
+  name: 'app',
+  components: { Navigation },
   data() {
-    return {};
+    return {}
   },
   created() {},
   mounted() {},
   methods: {},
-  watch: {},
-};
+  watch: {}
+}
 </script>
 
 <style lang="scss">
-@import url("https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap");
+@import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap');
 
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-  font-family: "Quicksand", sans-serif;
+  font-family: 'Quicksand', sans-serif;
 }
 
 .app {
